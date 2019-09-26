cbba2f5b454d2bfff4e8d48c0b1c1ca9c9d7383b
alexandre-mace <contact.alexandremace@gmail.com>
2019-09-26 12:24:47 +0200

    [API] Update join demand entity
    added serialization groups and max depth because it was triggering max depth
    error

--- api/src/Entity/JoinDemand.php
+++ api/src/Entity/JoinDemand.php
@@ -4,10 +4,18 @@ namespace App\Entity;
 
 use ApiPlatform\Core\Annotation\ApiResource;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Serializer\Annotation\MaxDepth;
 
 /**
- * @ApiResource()
- * @ORM\Entity(repositoryClass="App\Repository\JoinDemandRepository")
+/**
+ * @ApiResource(
+ *     attributes={
+ *         "formats"={"jsonld"},
+ *         "normalization_context"={"groups"={"joinDemand", "joinDemand:read"}, "enable_max_depth"=true},
+ *         "denormalizationContext"={"groups"={"joinDemand", "joinDemand:write"}, "enable_max_depth"=true}
+ *     },
+ * ) * @ORM\Entity(repositoryClass="App\Repository\JoinDemandRepository")
  */
 class JoinDemand
 {
@@ -15,23 +23,28 @@ class JoinDemand
      * @ORM\Id()
      * @ORM\GeneratedValue()
      * @ORM\Column(type="integer")
+     * @Groups({"project", "joinDemand"})
      */
     private $id;
 
     /**
      * @ORM\ManyToOne(targetEntity="App\Entity\User", inversedBy="joinDemands")
      * @ORM\JoinColumn(nullable=false)
+     * @Groups({"user", "project", "joinDemand"})
      */
     private $demander;
 
     /**
      * @ORM\ManyToOne(targetEntity="App\Entity\Project", inversedBy="joinDemands")
      * @ORM\JoinColumn(nullable=false)
+     * @MaxDepth(1)
+     * @Groups({"user", "joinDemand"})
      */
     private $relatedProject;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Groups({"user", "project", "joinDemand"})
      */
     private $status;
 
@@ -66,7 +79,13 @@ class JoinDemand
 
     public function getStatus(): ?string
     {
-        return $this->status;
+        switch ($this->status) {
+            case 'pending':
+                return 'En attente';
+                break;
+            default:
+                return $this->status;
+        }
     }
 
     public function setStatus(string $status): self
