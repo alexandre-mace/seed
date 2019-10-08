670e1cbb165089887a0e1c18bbddc39c012a5a8d
alexandre-mace <contact.alexandremace@gmail.com>
2019-10-08 21:23:31 +0200

    Updates

--- precache-manifest.82895f5addab1f6861c152eb585e5e06.js
+++ precache-manifest.82895f5addab1f6861c152eb585e5e06.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = [
   {
-    "revision": "674f50d287a8c48dc19ba404d20fe713",
-    "url": "/together/static/media/fontawesome-webfont.674f50d2.eot"
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/together/static/media/fontawesome-webfont.af7ae505.woff2"
   },
   {
-    "revision": "76d9c16c550c0ecc9236",
+    "revision": "3b77f5f5ad87dd91130a",
     "url": "/together/static/css/main.ed1006f1.chunk.css"
   },
   {
@@ -20,16 +20,16 @@ self.__precacheManifest = [
     "url": "/together/static/js/2.9a2e5ee1.chunk.js"
   },
   {
-    "revision": "76d9c16c550c0ecc9236",
-    "url": "/together/static/js/main.8b207ce3.chunk.js"
+    "revision": "3b77f5f5ad87dd91130a",
+    "url": "/together/static/js/main.03b45918.chunk.js"
   },
   {
     "revision": "fee66e712a8a08eef5805a46892932ad",
     "url": "/together/static/media/fontawesome-webfont.fee66e71.woff"
   },
   {
-    "revision": "af7ae505a9eed503f8b8e6982036873e",
-    "url": "/together/static/media/fontawesome-webfont.af7ae505.woff2"
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/together/static/media/fontawesome-webfont.674f50d2.eot"
   },
   {
     "revision": "b06871f281fee6b241d60582ae9369b9",
@@ -40,7 +40,7 @@ self.__precacheManifest = [
     "url": "/together/static/css/2.dde1641f.chunk.css"
   },
   {
-    "revision": "7578c43de6eca0653efc7d8d9d5764cc",
+    "revision": "921863296356edb7af97782d9593b64d",
     "url": "/together/index.html"
   }
 ];
