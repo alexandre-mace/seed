61cbfb4600dc3e6f6c4d3bcc7e7826d8f2a2b0f5
alexandre-mace <contact.alexandremace@gmail.com>
2019-10-09 13:19:39 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/together/precache-manifest.e4b45e528beae234eb6ec775f24ea233.js"
+  "/together/precache-manifest.080e6462def2e1b1d2e6afbc134022ee.js"
 );
 
 workbox.clientsClaim();
