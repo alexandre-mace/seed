48ce172daa49701976a42dda938c8194da5f3586
alexandre-mace <contact.alexandremace@gmail.com>
2019-10-11 20:11:43 +0200

    [CLIENT] Simplify my project component

--- client/src/components/dashboard/MyProject.js
+++ client/src/components/dashboard/MyProject.js
@@ -1,61 +1,9 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
 import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
 import MyProjectMembers from "./MyProjectMembers";
 import MyProjectDemands from "./MyProjectDemands";
-import {Link} from "react-router-dom";
-
-function TabPanel(props) {
-  const { children, value, index, ...other } = props;
-
-  return (
-    <Typography
-      component="div"
-      role="tabpanel"
-      hidden={value !== index}
-      id={`full-width-tabpanel-${index}`}
-      aria-labelledby={`full-width-tab-${index}`}
-      {...other}
-    >
-      <Box p={3}>{children}</Box>
-    </Typography>
-  );
-}
-
-TabPanel.propTypes = {
-  children: PropTypes.node,
-  index: PropTypes.any.isRequired,
-  value: PropTypes.any.isRequired,
-};
-
-function a11yProps(index) {
-  return {
-    id: `full-width-tab-${index}`,
-    'aria-controls': `full-width-tabpanel-${index}`,
-  };
-}
-
-const useStyles = makeStyles(theme => ({
-  root: {
-    flexGrow: 1,
-    width: '100%',
-    backgroundColor: theme.palette.background.paper,
-  },
-}));
 
 export default function MyProject(props) {
-  const classes = useStyles();
-  const [value, setValue] = React.useState(0);
-
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
-
   return (
     <div className="d-flex flex-column flex-md-row justify-content-around">
       <div className="d-flex flex-column">
@@ -66,10 +14,10 @@ export default function MyProject(props) {
       </div>
       <div className="d-flex flex-column">
         <Typography variant="subtitle2">
-        Les membres
+          Les membres
         </Typography>
         <MyProjectMembers {...props} project={props.project} />
       </div>
-</div>
-);
+    </div>
+  );
 }
